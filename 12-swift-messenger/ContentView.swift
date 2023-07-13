b785312f464700ae59b1ab8acbb7885edf846808
Liu Ziyi <ziyi.liu02@gmail.com>
2023-07-13 15:28:49 +0800

    Initial Commit

---
+++ 12-swift-messenger/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  12-swift-messenger
+//
+//  Created by Liu Ziyi on 13/7/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 12-swift-messenger/_2_swift_messengerApp.swift
@@ -0,0 +1,17 @@
+//
+//  _2_swift_messengerApp.swift
+//  12-swift-messenger
+//
+//  Created by Liu Ziyi on 13/7/23.
+//
+
+import SwiftUI
+
+@main
+struct _2_swift_messengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
